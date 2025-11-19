90745c4db2b2d1ecca8c25527914bd8bc8562214
chehachraf <achrafchehboun@outlook.com>
2025-11-19 14:41:15 +0100

    add the service class with one saving method

--- src/main/java/com/trans/everest/domain/iam/repository/UserRepository.java
+++ src/main/java/com/trans/everest/domain/iam/repository/UserRepository.java
@@ -5,10 +5,9 @@ import com.trans.everest.domain.iam.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User,String> {
-    Optional<User> findByLogin(String login);
+    boolean findByLogin(String login);
 
     boolean existsByLogin(String login);
 
--- src/main/java/com/trans/everest/domain/iam/service/UserServiceImpl.java
+++ src/main/java/com/trans/everest/domain/iam/service/UserServiceImpl.java
@@ -1,4 +1,33 @@
 package com.trans.everest.domain.iam.service;
 
-public class UserServiceImpl {
+import com.trans.everest.domain.iam.dto.RegisterUserRequest;
+import com.trans.everest.domain.iam.dto.UserResponse;
+import com.trans.everest.domain.iam.mapper.UserMapper;
+import com.trans.everest.domain.iam.model.User;
+import com.trans.everest.domain.iam.repository.UserRepository;
+import com.trans.everest.domain.iam.service.interfaces.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    @Transactional
+    public UserResponse registerUser(RegisterUserRequest request) {
+        if(userRepository.findByLogin(request.login())){
+            throw new RuntimeException("this login is already used");
+        }
+        User user = userMapper.toEntity(request);
+        user.setPassword(passwordEncoder.encode(request.password()));
+        User savedUser = userRepository.save(user);
+        return userMapper.toResponse(savedUser);
+    }
 }
