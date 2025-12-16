56e40416fcb2e970baad991e0e634eef817f5e72
chehachraf <achrafchehboun@outlook.com>
2025-12-16 11:10:01 +0100

    adding User Details Service Implementation

---
+++ src/main/java/com/trans/everest/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.trans.everest.infrastructure.security;
+
+import com.trans.everest.domain.iam.model.User;
+import com.trans.everest.domain.iam.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // البحث عن المستخدم
+        User user = userRepository.findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with login: " + username));
+
+        // تحويل المستخدم إلى صيغة Spring Security
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getLogin())
+                .password(user.getPassword())
+                .authorities(user.getRole().name())
+                .build();
+    }
+}
